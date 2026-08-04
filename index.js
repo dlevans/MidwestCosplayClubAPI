9ca654dc8311821a4801bb24935a4d7e736248bc
dustin evans <evansdustin@hotmail.com>
2026-08-03 22:22:36 -0500

    scavenger hunt take 2

--- index.js
+++ index.js
@@ -35,7 +35,7 @@ const tshirtRoutes = require("./routes/tshirtRoutes");
 const copyrightRoutes = require("./routes/copyrightRoutes");
 
 //Scav route
-const huntRoute = require("/huntRoute");
+const huntRoute = require("./huntRoute");
 
 // Use routes
 app.use("/login", authRoutes);
